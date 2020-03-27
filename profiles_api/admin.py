7d30a9732c6c0062de4e32c9452b1710c00663fb
michaldobiezynski <dobiezynski@gmail.com>
2020-03-27 13:15:44 +0000

    fixed typo

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -5,4 +5,3 @@ from profiles_api import models
 
 admin.site.register(models.UserProfile)
 
-
--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-
-from profile_api import views
+from profiles_api import views
 
 
 urlpatterns = [
